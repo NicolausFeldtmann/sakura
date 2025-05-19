69da131ce2a531bc28eafc49b83046a65f797db2
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-05-19 11:22:27 +0200

    first commit

---
+++ sakura/src/app/main-content/main-content.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [
+    LandingPageComponent,
+    HowToOrderComponent,
+    OurMenuComponent
+  ],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
